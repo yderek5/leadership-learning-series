71c50fb581809cfd9fcaa728e867826cf57a9424
Seth Walton <sdwalton83@gmail.com>
2018-02-28 01:16:41 -0600

    just added JSX for Login still needs logic

--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -3,7 +3,18 @@ import './login.css';
 
 const Login = (props) => {
     return(
-        <h2>Login</h2>
+        <form onSubmit={this.onSubmit} className="container">
+        <h1> Login</h1>
+        <div className="form-group"></div>
+        <label htmlFor="username">Username:</label>
+        <input onChange={this.onChange} id="username" type="text" placeholder="Enter Username" name="username" className="form-control" />
+        <div className="form-group"></div>
+        <label htmlFor="password">Password:</label>
+        <input onChange={this.onChange} id="password" type="password" placeholder="Enter Password" name="password" className="form-control" />
+        <div className="form-group"></div>
+        <br />
+        <button type="submit" className="btn btn-primary">LogIn</button>
+        </form>
     )
 }
 
