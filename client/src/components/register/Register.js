e6b243acb2cb69e7ba9d6268baf290f99a83c2be
Seth Walton <sdwalton83@gmail.com>
2018-02-28 02:39:48 -0600

    update mLab link

--- client/src/components/register/Register.js
+++ client/src/components/register/Register.js
@@ -11,7 +11,7 @@ class Register extends React.Component {
             username: '',
             password: '',
             confirmPassword: '',
-            mail: false // boolean not working trying a string
+            mail: false 
         }
     }
 
