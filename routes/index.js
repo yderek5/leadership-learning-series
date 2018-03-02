4374f6d6308fd015e6513abf2ae26cd99a80deda
yderek5 <yderek5@gmail.com>
2018-03-01 22:19:42 -0600

    heroku test

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,10 @@ var router = express.Router();
 var ExpressValidator = require('express-validator');
 router.use(ExpressValidator());
 
-/* This is just here to check if the backend is running */
-router.get('/', function(req, res, next) {
-    res.send('This is the express backend server');
-});
+/* This Serves our React app */
+app.get("*", (req, res) => {  
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
 
 // post route for contact form
 router.post('/contact/api', require('../logic/contactForm').sendEmail);
