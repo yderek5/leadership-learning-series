394386cc3911a339bf7e281401b61bb86296be25
yderek5 <yderek5@gmail.com>
2018-03-01 22:42:41 -0600

    fix

--- app.js
+++ app.js
@@ -22,7 +22,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'client', 'build')));
 
-app.use('/', index);
+app.get("*", (req, res) => {  
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
