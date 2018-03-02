afcb4f879578d02d46957dbb1098991d877c652a
yderek5 <yderek5@gmail.com>
2018-03-01 22:33:47 -0600

    fix

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,7 @@ var ExpressValidator = require('express-validator');
 router.use(ExpressValidator());
 
 /* This Serves our React app */
-router.get("*", (req, res) => {  
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  });
+
 
 // post route for contact form
 router.post('/contact/api', require('../logic/contactForm').sendEmail);
