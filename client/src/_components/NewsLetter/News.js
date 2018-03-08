673b5beefcc4e0114933fdf7333f839ae07eed99
Seth Walton <sdwalton83@gmail.com>
2018-03-08 17:24:42 -0600

    navbar option for mailchimp

---
+++ client/src/_components/NewsLetter/News.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './news.css';
+
+const News = props => {
+	return (
+		<div className="container">
+			<h2>News Letter</h2>
+			<p>
+				If you would like to be a part of our Network please sign up for
+				our News Letter. Helpful Resume tips, possible job leads, and
+				lots more will be mailed once a month to keep everyone
+				connected.
+			</p>
+			<a className="btn btn-primary" href="http://eepurl.com/dnm74f">Click Here To Sign up for The Nesletter</a>
+		</div>
+	);
+};
+
+export default News;
--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: process.env.MONGODB,
-    secret: process.env.SECRET
+    database: 'mongodb://seth:admin@ds249798.mlab.com:49798/mailchimptest',
+    secret: 'mysecret'
 }
