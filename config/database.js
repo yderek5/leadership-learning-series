e09df50f3d54530657427d97f253256f8298656a
Seth Walton <sdwalton83@gmail.com>
2018-03-12 15:59:40 -0500

    change proccess.env

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: 'mongodb://admin:admin@ds153609.mlab.com:53609/leadershiplearningseries',
-    secret: 'mysecret'
+    database: process.env.MONGODB,
+    secret: process.env.SECRET
 }
