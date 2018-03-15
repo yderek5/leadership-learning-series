ce41f5b1e3369aa12ba3604b36805f846f91bc0d
Seth Walton <sdwalton83@gmail.com>
2018-03-14 19:08:48 -0500

    push

--- config/database.js
+++ config/database.js
@@ -1,8 +1,8 @@
-// module.exports = {
-//     database: process.env.MONGODB,
-//     secret: process.env.SECRET
-// }
 module.exports = {
-    database: 'mongodb://admin:seth@ds147668.mlab.com:47668/leadershiplearning',
-    secret: 'mysecret'
+    database: process.env.MONGODB,
+    secret: process.env.SECRET
 }
+// module.exports = {
+//     database: 'mongodb://admin:seth@ds147668.mlab.com:47668/leadershiplearning',
+//     secret: 'mysecret'
+// }
